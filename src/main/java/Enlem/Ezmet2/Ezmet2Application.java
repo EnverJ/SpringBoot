3d79319f1af59b6dde8f1144772b6df8b8d429ca
EnverJ <Enlem1987@gmail.com>
2022-07-29 23:27:19 -0600

    spring boot

---
+++ src/main/java/Enlem/Ezmet2/Ezmet2Application.java
@@ -0,0 +1,14 @@
+package Enlem.Ezmet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ezmet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ezmet2Application.class, args);
+		System.out.println("This is spring boot");
+	}
+
+}
---
+++ src/test/java/Enlem/Ezmet2/Ezmet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Enlem.Ezmet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ezmet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
