12208be284ff45f7f90a9a2efa0d1d386413a449
EnverJ <Enlem1987@gmail.com>
2022-08-01 22:10:58 -0600

    Rest Controller

--- src/main/java/Enlem/Ezmet2/Ezmet2Application.java
+++ src/main/java/Enlem/Ezmet2/Ezmet2Application.java
@@ -8,7 +8,7 @@ public class Ezmet2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ezmet2Application.class, args);
-		System.out.println("This is spring boot");
+		System.out.println("This is spring boot.    Need more practice");
 	}
 
 }
---
+++ src/main/java/Enlem/Ezmet2/userController.java
@@ -0,0 +1,37 @@
+package Enlem.Ezmet2;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class userController {
+	@GetMapping
+	public String getUsers() {
+
+		return "http GET request was sent";
+	}
+
+	@PostMapping
+	public String createUsers() {
+
+		return "http POST request was sent";
+	}
+
+	@PutMapping
+	public String updateUsers() {
+
+		return "http PUT request was sent";
+	}
+
+	@DeleteMapping
+	public String deleteUsers() {
+
+		return "http DELTE request was sent";
+	}
+
+}
